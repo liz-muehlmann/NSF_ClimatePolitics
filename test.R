38cb6508d5ca66dce5c884d934ce0c556c97acd6
liz <liz.muehlmann@uci.edu>
2024-07-26 20:31:07 -0700

    flextable testing

--- test.R
+++ test.R
@@ -10,12 +10,30 @@ load("./LocalView/data/modified/lv_clean_noTranscript.rdata")
 load("./LocalView/data/modified/lv_countyYear_noNA.rdata")
 load("./LocalView/data/modified/allData_state.rdata")
 
-lv <- lvClean_noScript %>% 
+ca <- lvClean_noScript %>% 
     select(transcript_year, state_name, county_name, n_ccMentions, ccBinary) %>% 
+    filter(state_name == "California") %>% 
     group_by(transcript_year, county_name) %>% 
     summarize(n_script = n(),
               n_script_ccMention = sum(ccBinary),
-              total_ccMention = sum(n_ccMentions)) %>% 
+              total_ccMention = sum(n_ccMentions))
+
+%>% 
     pivot_wider(names_from = transcript_year, 
                 values_from = c(n_script, n_script_ccMention, total_ccMention))
 ### flextable
+
+ca %>% 
+  as_flextable()
+
+
+
+
+
+
+
+
+
+
+
+
