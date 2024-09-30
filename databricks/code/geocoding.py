d71963dc235cf5d868bd0d922c30aa954fe37f5c
liz <liz.muehlmann@uci.edu>
2024-09-30 15:52:57 -0700

    work on python - code is janky need to write to csv so it works with census

--- databricks/code/geocoding.py
+++ databricks/code/geocoding.py
@@ -4,7 +4,6 @@ from pyspark.sql import SparkSession
 from pyspark.sql.window import Window
 import tempfile, os, csv, io, requests as r, pandas as pd
 
-
 ################################################################################
 ## the following section is only necessary when using the sample data hosted  ##
 ## on a local machine. Skip to the next section when transporting this        ##
@@ -12,10 +11,11 @@ import tempfile, os, csv, io, requests as r, pandas as pd
 ################################################################################
 
 ## change working directory
-os.chdir("F:/GitHub/NSF_ClimatePolitics/databricks/")
+# os.chdir("F:/GitHub/NSF_ClimatePolitics/databricks/")
+os.chdir("C:/Users/lizmo/Documents/GitHub/NSF_ClimatePolitics/databricks/")
 
 ## import csv
-geo_tx = pd.read_csv("./data/l2sample_tx.csv")
+geo_tx = pd.read_csv("./data/l2sample_tx.csv", low_memory = False)
 
 geo_tx = geo_tx.assign(row_index=range(len(geo_tx)))
 
@@ -34,7 +34,6 @@ geo_tx = geo_tx.assign(row_index=range(len(geo_tx)))
 
 # ______________________________________________________________________________
 # define geocoding function                                                 ####
-
 
 def batch_geocode(df):
     # subset the row index and address columns from geo_tx
@@ -92,46 +91,55 @@ temp_dir = "./data/temp/"
 
 try:
     # set temporary file name
-    temp_fn = "temp_addresses.csv"
+    # temp_fn = "temp_addresses.csv"
 
     # set temporary file path
-    temp_fp = os.path.join(temp_dir, temp_fn)
+    # temp_fp = os.path.join(temp_dir, temp_fn)
 
     # create temporary file
-    temp_file = tempfile.NamedTemporaryFile(delete=False, dir=temp_dir, prefix=temp_fn)
+    # temp_file = tempfile.NamedTemporaryFile(delete=False, dir=temp_dir, prefix=temp_fn)
 
     # write address df to temporary file
-    addresses.to_csv(temp_file, header=False, index=False)
+    # addresses.to_csv(temp_file, header=False, index=False, index_label=False)
+
+    with tempfile.NamedTemporaryFile(delete=False, suffix='.csv') as temp_file:
+    # Step 3: Write the DataFrame to the temporary CSV
+        addresses.to_csv(temp_file.name, index=False, header=False, index_label=False)
+    
+    # Print the name of the temporary file
+        print(f'Temporary CSV file created at: {temp_file.name}')
 
 # error handling
 except Exception as e:
     print(f"Danger, Will Robinson! {str(e)}")
 
+## 
+d = pd.read_csv(temp_file.name)
+os.remove(temp_file)
+
 # define api url
-apiurl = "https://geocoding.geo.census.gov/geocoder/geographies/addressbatch"
+apiurl = 'https://geocoding.geo.census.gov/geocoder/geographies/addressbatch'
 
 # define upload file
-upload_file = {'file': open(temp_file.name, 'rb')}
+upload_file = {'addressFile': (temp_file.name, open(temp_file.name, 'rb'), 'text/csv')}
 
 # define data to request from census
-data_request = {'addressFile': upload_file,
+data_request = {
            'benchmark': 'Public_AR_Current',
-           'vintage': 'Census2020_Current'}
+           'vintage': 'Census2020_Current'
+           }
 
-# create empty list for bad lines
-
-bad_line_jail = []
 # define bad lines function
+bad_line_jail = []
 def go_to_jail(line):
     bad_line_jail.append(line)
 
-
 try:
     # save census response data
-    sd = r.post(apiurl, data = data_request)
+    sd = r.post(apiurl, files = upload_file, data = data_request)
     
     # convert response option to pandas df
-    df = pd.read_csv(io.StringIO(sd.text), sep = ",", header = None, quoting = csv.QUOTE_ALL, on_bad_lines = go_to_jail, engine = 'python')
+    return_df = pd.read_csv(io.StringIO(sd.text), sep = ",", header = None, quoting = csv.QUOTE_ALL, on_bad_lines = go_to_jail, engine = 'python')
     
     # make the data pretty
     with pd.option_context(
@@ -140,14 +148,24 @@ try:
         'display.max_colwidth', -1,
         'display.colheader_justify', 'left'
     ):
-        print(df)
+        print(return_df)
+    return_df = ['ID', 'ADDRESS_IN', 'MATCH_INDICATION', 'MATCH_TYPE', 'ADDRESS_OUT', 'LONG_LAT', 'TIGER_EDGE', 'STREET_SIDE', 'FIPS_STATE', 'FIPS_COUNTY', 'CENSUS_TRACT', 'CENSUS_BLOCK']
 except ValueError as ve:
     bad_line_jail.append(ve)
 except Exception as e:
     print(f"Oh no! Something went wrong: {str(e)}")
 
+
+return_df.head()
+
+
+
+
+
+
 ## remove temporary file
-os.remove(temp_fp)
+temp_file.close()
+os.remove(temp_file.name)
 
 
 print(bad_line_jail)
